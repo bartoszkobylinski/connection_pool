a0ab9ccc04933e9017a7cb25a6931b2154f01b89
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-12-20 16:10:14 +0100

    rounding some input and adding some lines to get better ux

--- conn_pool_threading.py
+++ conn_pool_threading.py
@@ -1,9 +1,7 @@
 import random
-import threading
 import concurrent.futures
 import time
-from connection_pool import ConnectionPool, DBConnection
-from random import randint
+from connection_pool import ConnectionPool
 
 connection_pool = ConnectionPool()
 
@@ -15,17 +13,17 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     while delta_time < 60:
         current_time = time.time()
         delta_time = current_time - start_time
-        #executor.submit(counting)
-        #print(f"I'm before loop and counter is: {counter} ")
-        random_number = random.randint(1,150)
+        random_number = random.randint(1,110)
         for _ in range(random_number):
             executor.submit(connection_pool.get_connection)
             connection_pool.check_pool()
             connection_pool.terminate_unused_connection()    
-        time.sleep(1)
+        time.sleep(2)
+        print('----------------------------------------')
         print(f"available connection: {len(connection_pool.connection_pool['available'])} used connection: {len(connection_pool.connection_pool['used'])}")
-        print(f"{random_number} threads is running now and {delta_time}s has passed since the beggining. Procees will stop after 60s")    
+        print(f"{random_number} threads is running now and {round(delta_time,1)}s has passed since the beggining. Test case will stop after 60s")    
         print("\n")
+        time.sleep(1)
             
     
     
